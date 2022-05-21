3606e2d1bfe2d487d59bca19cedfaa755c4cf4cb
Keito Kubo <89447369+KeitoKubo@users.noreply.github.com>
2022-05-21 14:43:31 +0900

    Create ABC251_D.cpp

---
+++ 水色/ABC251_300/ABC251_D.cpp
@@ -0,0 +1,125 @@
+#define _USE_MATH_DEFINES
+#include <iostream>                         // cout, endl, cin
+#include <cstdio>                           // printf, scanf
+#include <string>                           // string, to_string, stoi
+#include <algorithm>                        // min, max, swap, lower_bound, upper_bound
+// & stable_sort, sort, reverse
+#include <cmath>                            //sin(rad),cos,tan, abs, pow, sqrt, cbrt,  exp, log, log10, log2
+#include <utility>                          // pair, make_pair
+#include <map>                              // map
+#include <set>                              // set
+#include <vector>                           // vector
+#include <queue>                            // queue, priority_queue
+#include <stack>                            // stack
+#include <list>                             // list
+#include <deque>                            // deque
+#include <iomanip>
+#include <numeric>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef vector<int> vi;
+typedef vector<string> vs;
+typedef pair<int, int> pii;
+typedef pair<string, int> psi;
+typedef pair<int, string> pis;
+typedef set<int> si;
+typedef map<string, int> msi;
+typedef map<string, bool> msb;
+typedef priority_queue<int> pqi;
+typedef stack<int> sti;
+typedef queue<int> qi;
+
+#define infi 2147483647
+#define infl 9223372036854775806
+#define MOD  1000000007
+#define EPS 0.0000000001
+#define rep(i,n) for(int i=0;i<(int)n;i++)
+#define repa(i,n) for(int i=1;i<=(int)n;i++)
+#define irep(i,n) for(int i=(int)n-1;i>=0;i--)
+
+//Union Find Tree
+class DisjointSet {
+public:
+	vi rank, p; //rankはその木の深さ、pは親ノード
+
+	DisjointSet() {}
+	DisjointSet(int size) {
+		rank.resize(size, 0);
+		p.resize(size, 0);
+		rep(i, size) makeSet(i);
+	}
+
+	void makeSet(int x) {
+		p[x] = x;
+		rank[x] = 0;
+	}
+
+	int findSet(int x) {
+		if (x != p[x]) p[x] = findSet(p[x]);
+		return p[x];
+	}
+
+	bool same(int x, int y) {
+		if (findSet(x) == findSet(y)) return true;
+		else return false;
+	}
+
+	void link(int x, int y) {
+		if (rank[x] > rank[y]) p[y] = x;
+		else {
+			p[x] = y;
+			if (rank[x] == rank[y]) rank[y]++;
+		}
+	}
+
+	void unite(int x, int y) {
+		link(findSet(x), findSet(y));
+	}
+};
+
+//sort vector<pii> by second element
+bool compare_by_b(pair<int, int> a, pair<int, int> b) {
+	if (a.second != b.second) {
+		return a.second < b.second;
+	}
+	else {
+		return a.first < b.first;
+	}
+}
+
+//---------------------------------------------------
+//今回のソリューションの計算量はO(N^2logN)です
+
+const int max_n = 1005;
+int g[max_n]; //整数iの約数の個数
+int num[max_n][max_n]; //整数iがjのnum[i][j]-1乗を因数として持つ
+bool isprime[max_n]; //整数iが素数かどうか
+int n, t;
+
+//xとyの最大公約数を求める
+int gcd(int x, int y) {
+	if (x % y == 0) return y;
+	else return gcd(y, x % y);
+}
+
+int main() {
+	//int a = in<int>(); string b = in<string>(); char c = in<char>();
+	//double d = in<double>();
+	//(void)scanf("%d",&);
+	
+	int w; cin >> w;
+	cout << 298 << endl;
+
+	repa(i, 99) cout << i << " ";
+	for (int i = 100; i < 10000; i += 100) cout << i << " ";
+	for (int i = 10000; i < 1000000; i += 10000) cout << i << " ";
+	cout << 1000000 << endl;
+
+	return 0;
+}
